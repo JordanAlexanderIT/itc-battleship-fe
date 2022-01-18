965f659f0f7f3dab0c7edf9c6e70fd32960a33fe
AlexanderTkach <Alexander_tkach@mail.ru>
2022-01-18 10:02:44 +0200

    some updates to the Play component

--- src/Components/Ships/OneCellShip.jsx
+++ src/Components/Ships/OneCellShip.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import Cell from '../Cell';
+import { useDrag } from 'react-dnd';
 
-const OneCellShip = () => {
+const OneCellShip = ({id}) => {
+
+    const [{isDragging}, drag] = useDrag(()=> ({
+        type: 'ship',
+        item: {id: id},
+        collect: monitor => ({
+            isDragging: !!monitor.isDragging(),
+        }),
+    }));
+
     return (
-        <div className='OneCellShip'>
+        <div className='OneCellShip' ref={drag} style={{border: isDragging ? "5px solid pink" : "0px"}}> 
            <Cell /> 
         </div>
     );
--- src/Components/Ships/TwoCellShip.jsx
+++ src/Components/Ships/TwoCellShip.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import Cell from '../Cell';
+import { useDrag } from 'react-dnd';
 
-const TwoCellShip = () => {
+const TwoCellShip = ({id}) => {
+
+    const [{isDragging}, drag] = useDrag(()=> ({
+        type: 'ship',
+        item: {id: id},
+        collect: monitor => ({
+            isDragging: !!monitor.isDragging(),
+        }),
+    }));
+
     return (
-        <div className='TwoCellShip'>
+        <div className='TwoCellShip'ref={drag} style={{border: isDragging ? "5px solid pink" : "0px"}}>
            <Cell /> 
            <Cell />
         </div>
